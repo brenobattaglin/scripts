d5c39ab16e9e6b36b74a4f8204b92c774cc4d8ee
Breno Battaglin <breno.battaglin@icloud.com>
2022-03-05 11:23:19 -0300

    Update backup rule for Dev dir

--- rsync/backup-home-folders.sh
+++ rsync/backup-home-folders.sh
@@ -2,10 +2,10 @@
 
 destination=~/Backup/M1/
 
-rsync -vaz ~/Desktop "${destination}"
-rsync -vaz ~/Dev "${destination}"
-rsync -vaz ~/Documents "${destination}"
-rsync -vaz ~/Downloads "${destination}"
-rsync -vaz ~/Movies "${destination}"
-rsync -vaz ~/Music "${destination}"
-rsync -vaz ~/Pictures "${destination}"
+rsync -vaz ~/Desktop ${destination}
+rsync -vaz --exclude=node_modules/ ~/Dev targetHost:${destination}
+rsync -vaz ~/Documents ${destination}
+rsync -vaz ~/Downloads ${destination}
+rsync -vaz ~/Movies ${destination}
+rsync -vaz ~/Music ${destination}
+rsync -vaz ~/Pictures ${destination}
