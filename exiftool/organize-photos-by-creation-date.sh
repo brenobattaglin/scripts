beba939c921dfbc1d689241afcfa62840cdb821d
Breno Battaglin <breno.battaglin@icloud.com>
2022-04-18 14:32:27 -0300

    Update photo script
    - Sort just by year

--- exiftool/organize-photos-by-creation-date.sh
+++ exiftool/organize-photos-by-creation-date.sh
@@ -2,4 +2,4 @@
 
 unorganized_photos_dir=~/Desktop/photos
 
-exiftool '-Directory<CreateDate' -d ~/Desktop/%Y-%m-%d -r $unorganized_photos_dir
+exiftool '-Directory<CreateDate' -d ~/Desktop/%Y -r $unorganized_photos_dir
