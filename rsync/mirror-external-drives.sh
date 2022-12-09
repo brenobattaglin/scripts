cc90f905eb126a0a68df0c9fbf66ffca62662fae
Breno Battaglin <breno.battaglin@icloud.com>
2022-12-09 20:43:53 -0300

    Update mirror drives sh

--- rsync/mirror-external-drives.sh
+++ rsync/mirror-external-drives.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-# 1TB external hard drive
 destination1=/Volumes/Untitled/
-
-# 500GB external hard drive
 destination2=/Volumes/Untitled\ 1/
 
-rsync -av --delete --progress $destination1 $destination2
+rsync -Rvaz --progress $destination1 $destination2
