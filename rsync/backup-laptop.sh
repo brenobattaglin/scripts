4619d4f2f2a1bd8a527eb3962af5b48452963bbb
Breno Battaglin <breno.battaglin@icloud.com>
2022-12-26 12:00:30 -0300

    Update backup script
    - Add paths
    - Rename script file

--- rsync/backup-laptop.sh
+++ rsync/backup-laptop.sh
@@ -4,10 +4,22 @@ destination=/Volumes/Untitled/Backup/M1/
 
 mkdir -p $destination
 
+
 rsync -Rva --progress ~/Desktop $destination
-rsync -Rva --progress --exclude=node_modules/ ~/Dev targetHost:$destination
+
+rsync -Rva --progress --exclude=node_modules/ ~/Developer targetHost:$destination
+
 rsync -Rva --progress ~/Documents $destination
+
 rsync -Rva --progress ~/Downloads $destination
+
+rsync -Rva --progress ~/Games $destination
+
+//iCloud
+rsync -Rva --progress --exclude=.Trash/ ~/Library/Mobile\ Documents/com~apple~CloudDocs $destination
+
 rsync -Rva --progress ~/Movies $destination
+
 rsync -Rva --progress ~/Music $destination
+
 rsync -Rva --progress ~/Pictures $destination
